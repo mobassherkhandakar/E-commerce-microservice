5e89c936e0b2563eeabbfa1b0843f3ec2a065b7f
mobassherkhandakar <mobassherkhandakar@gmail.com>
2024-10-30 13:12:19 +0600

    api-gatewaye intrigate

---
+++ api-geteway/src/utils.ts
@@ -0,0 +1,39 @@
+import { Express, Request, Response } from "express"
+import config from './config.json';
+import { hostname } from "os";
+import axios from "axios";
+
+export const createHandler = (hostname: string, path: string, method: string) => {
+    return async (req: Request, res: Response) => {
+        try {
+            let url = `${hostname}${path}`
+            Object.keys(req.params).forEach(param => {
+                url = url.replace(`:${param}`, req.params[param])
+            })
+            const { data } = await axios({
+                method,
+                url,
+                data: req.body
+            })
+            res.json(data)
+        } catch (error) {
+            if (error instanceof axios.AxiosError) {
+                return res.status(error.response?.status || 500)
+                    .json(error.response?.data)
+            } else {
+                return res.status(500).json({ message: "Internal Server Error" })
+            }
+        }
+    }
+}
+export const configureRoutes = (app: Express) => {
+    Object.entries(config.services).forEach(([_name, service]) => {
+        const hostname = service.url;
+        service.routes.forEach((route) => {
+            route.methods.forEach(method => {
+                const handler = createHandler(hostname, route.path, method)
+                app[method](`/api${route.path}`, handler)
+            })
+        })
+    })
+}

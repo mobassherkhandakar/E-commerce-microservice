9d98bb94befc841e988e6ac7c9e7cd91e30659f3
mobassherkhandakar <mobassherkhandakar@gmail.com>
2024-11-13 03:08:02 +0600

    basic server done

--- api-geteway/src/utils.ts
+++ api-geteway/src/utils.ts
@@ -2,6 +2,7 @@ import { Express, Request, Response } from "express"
 import config from './config.json';
 import { hostname } from "os";
 import axios from "axios";
+import middlewares from "./middlewares";
 
 export const createHandler = (hostname: string, path: string, method: string) => {
     return async (req: Request, res: Response) => {
@@ -26,13 +27,17 @@ export const createHandler = (hostname: string, path: string, method: string) =>
         }
     }
 }
+export const getMiddlewares = (names: string[]) => {
+    return names.map(name => middlewares[name])
+}
 export const configureRoutes = (app: Express) => {
     Object.entries(config.services).forEach(([_name, service]) => {
         const hostname = service.url;
         service.routes.forEach((route) => {
             route.methods.forEach(method => {
+                const middleware = getMiddlewares(route.middlewares)
                 const handler = createHandler(hostname, route.path, method)
-                app[method](`/api${route.path}`, handler)
+                app[method](`/api${route.path}`, middleware, handler)
             })
         })
     })
